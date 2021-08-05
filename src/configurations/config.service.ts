33078a53062873ee9ba3410b09357ff60d124fd9
David Hovhannisyan <david@wesage.io>
2021-08-05 13:33:04 +0400

    Removed .env file from config service.

--- src/configurations/config.service.ts
+++ src/configurations/config.service.ts
@@ -1,42 +1,25 @@
-import { parse, DotenvParseOutput } from 'dotenv';
-import { readFileSync } from 'fs';
-
 export class ConfigService {
-    private readonly envConfig: DotenvParseOutput;
-
-    constructor(filePath: string) {
-        const parsedConfig = parse(readFileSync(filePath));
-        this.envConfig = parsedConfig;
+    constructor() {
+        // Empty constructor
     }
-
-    /**
-     * Generic getter
-     */
-    get(key: string) {
-        return this.envConfig[key];
-    }
-
-    /**
-     * Getters for each environment variable
-     */
 
     public get databaseType() {
-        return this.envConfig.DATABASE_TYPE;
+        return process.env.DATABASE_TYPE;
     }
 
     public get databaseHost() {
-        return this.envConfig.DATABASE_HOST;
+        return process.env.DATABASE_HOST;
     }
 
     public get databasePort() {
-        return Number(this.envConfig.DATABASE_PORT);
+        return Number(process.env.DATABASE_PORT);
     }
 
     public get databaseName() {
-        return this.envConfig.DATABASE_NAME;
+        return process.env.DATABASE_NAME;
     }
 
     public get jwtSecret() {
-        return this.envConfig.JWT_SECRET;
+        return process.env.JWT_SECRET;
     }
 }
